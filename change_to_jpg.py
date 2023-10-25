f48aae89d95a23f25fe15dbc05b9a66086e855f0
DucTriCE <trild.ce@gmail.com>
2023-10-26 01:53:21 +0700

    Fix change_name function

--- change_to_jpg.py
+++ change_to_jpg.py
@@ -1,23 +1,23 @@
 import os
-# Danh sách các lớp/tên thư mục
 
-# Đường dẫn thư mục chứa dữ liệu
-folder_path = './new_noise_data/'
+def rename_images_in_directory(directory_path):
+    if not os.path.exists(directory_path):
+        print(f"Thư mục '{directory_path}' không tồn tại.")
+        return
 
-# Duyệt qua từng lớp/thư mục
-count = 501                     #Set the begin index
+    image_files = [f for f in os.listdir(directory_path) if f.lower().endswith('.jpg')]
 
-# Duyệt qua từng tệp trong thư mục hiện tại
-for filename in os.listdir(folder_path):
+    if not image_files:
+        print(f"Không có tệp ảnh JPG nào trong thư mục '{directory_path}'.")
+        return
 
-    basename, extension = os.path.splitext(filename)
+    for i, image_file in enumerate(image_files):
+        new_name = f"{i}.jpg"  # Đổi tên thành '0.jpg', '1.jpg', ...
+        old_path = os.path.join(directory_path, image_file)
+        new_path = os.path.join(directory_path, new_name)
+        os.rename(old_path, new_path)
+        print(f"Đổi tên '{image_file}' thành '{new_name}'.")
 
-    old_path = os.path.join(folder_path, filename)
-    new_path = os.path.join(folder_path, str(count)+'.jpg')
-    
-    # Đổi tên tệp
-    os.rename(old_path, new_path)
-    
-    count += 1
-
-    print("Đổi tên các file thành công!")
+# Sử dụng hàm
+directory_path = "./sign_more_data"
+rename_images_in_directory(directory_path)
